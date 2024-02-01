e1a0ee9463143d265de23d945187878452dc8c67
DArzoEsp <davidarzola80@gmail.com>
2024-01-31 19:55:49 -0800

    finished most of it

--- components/card.jsx
+++ components/card.jsx
@@ -5,11 +5,14 @@ export default function Card() {
     return (
         <div className="card-container">
             <img className="card" src={photo} alt="pic of user img" ></img>
-            <p className='info'>
-                <div className='rating'><img className="star-info" src={star} alt="star icon" />  5.0 <span className='num'> (6) &#x2022; USA</span></div>
+            <h2 className='info'>
+                <div className='rating'>
+                    <img className="star-info" src={star} alt="star icon" />
+                    5.0 <span className='num'> (6) &#x2022; USA</span>
+                    </div>
                 Life Lessons with Katie Zaferes
                 <p><strong>From $136</strong> / person</p>
-            </p>
+            </h2>
         </div>
     )
 }
